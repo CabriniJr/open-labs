ac25314f723b038a751d163b3205b39dd510e5ad
CabrinJr <luigi.mendes.cabrini@outlook.com>
2026-08-28 13:04:58 -0300

    feat(depth-core): superficie publica do pacote

---
+++ packages/depth-core/src/index.ts
@@ -0,0 +1,4 @@
+export { Engine } from "./engine.js";
+export { diffStates } from "./diff.js";
+export { createRandom } from "./random.js";
+export type { LevelId, Scenario, StepContext } from "./types.js";
